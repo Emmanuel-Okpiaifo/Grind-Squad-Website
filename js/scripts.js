563768e8f435239193a7494b280bae7bde22e445
Emmanuel-Okpiaifo <108729387+Emmanuel-Okpiaifo@users.noreply.github.com>
2023-09-17 11:52:06 +0100

    adding html files, css, js and img folders

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const video = document.querySelector(".background-video");
+const fa = document.querySelector(".fa");
+const preloader = document.querySelector(".preloader");
+
+
+window.addEventListener("load", () => {
+  preloader.style.zIndex = "-999";
+});
